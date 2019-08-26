8c0cb0f14443b304df01dbb5c2b00b985094d0f8
zak <codedpills@gmail.com>
2019-08-26 00:21:35 +0000

    get fancier pokemon images from api

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (number) => number <= 999 ? `00${number}`.slice(-3) : number;
 
 export class Pokecard extends Component {
     render() {
         const { id, name, type, exp } = this.props;
-        let imgSrc = `${POKE_API}${id}.png`
+        const newId = padToThree(id);
+        let imgSrc = `${POKE_API}${newId}.png`
         return (
             <div className="pokecard">
                 <h1 className="pokecard-title">{name}</h1>
